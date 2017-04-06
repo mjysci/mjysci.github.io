5bb84e7e8a9b311252650a0e05542116e69ad18c
MA Junyi <mjysci@live.com>
2017-04-06 15:24:28 +0800

    Site updated: 2017-04-06 15:24:22

--- cardcalc/js/calc.js
+++ cardcalc/js/calc.js
@@ -22,8 +22,8 @@ function autoExc() {
     dataType: "jsonp",
     success: function(data) {
       $("#excRate").val(data.result.rate)
-      // console.log($("#excRate").value);
-      // console.log(data.result.rate)
+      //console.log($("#excRate").value);
+      //console.log(data.result.rate)
     },
     error: function() {
       alert('汇率请求失败');
@@ -31,33 +31,13 @@ function autoExc() {
   });
 }
 
-// 计算美元区售价，已经写到代码里面了
-// function calc() {
-//   var daoGot = rmbGot / excRate;
-//   var taxCamp = daoGot * 0.1;
-//   var taxValve = daoGot * 0.05;
-//   if (taxCamp > 0.01) {
-//     taxCamp = pointFloor(taxCamp);
-//   } else {
-//     taxCamp = 0.01;
-//   }
-//   if (taxValve > 0.01) {
-//     taxValve = pointFloor(taxValve);
-//   } else {
-//     taxValve = 0.01;
-//   }
-//   var daoSell = daoGot + taxCamp + taxValve;
-//   return daoSell
-// }
-
-function calcLoop() {
-  var rmbGot = document.getElementById("rmbGot").value;
-  var excRate = document.getElementById("excRate").value;
-  // console.log(rmbGot);
-  // console.log(excRate);
+function calcLoop(rmbGot, excRate) {
+  
+  //console.log(rmbGot);
+  //console.log(excRate);
   var daoGot = rmbGot / excRate;
   var daoGot = pointCeil(daoGot);
-  // console.log(daoGot);
+  //console.log(daoGot);
   var taxCamp = daoGot * 0.1;
   var taxValve = daoGot * 0.05;
   if (taxCamp > 0.01) {
@@ -70,15 +50,15 @@ function calcLoop() {
   } else {
     taxValve = 0.01;
   }
-  // console.log(taxCamp);
-  // console.log(taxValve);
+  //console.log(taxCamp);
+  //console.log(taxValve);
   var daoSell = daoGot + taxCamp + taxValve;
-  console.log(daoSell);
-  // console.log(daoSell);
+  //console.log(daoSell);
+  //console.log(daoSell);
   var initDao = daoSell;
   while (initDao === daoSell) {
     rmbGot = Number(rmbGot) + 0.01;
-    // console.log(rmbGot);
+    //console.log(rmbGot);
     var daoGot = rmbGot / excRate;
     var daoGot = pointCeil(daoGot);
     var taxCamp = daoGot * 0.1;
@@ -94,12 +74,26 @@ function calcLoop() {
       taxValve = 0.01;
     }
     var daoSell = Number(daoGot) + Number(taxCamp) + Number(taxValve);
-    console.log(initDao);
-    console.log(daoSell);
+    //console.log(initDao);
+    //console.log(daoSell);
   }
   rmbGot = Number(rmbGot) - 0.01;
-  // console.log(rmbGot);
-  document.getElementById("maxRmb").value = rmbGot
+  //console.log(rmbGot);
+  return rmbGot;
+}
+
+function main(){
+  var rmbGot = document.getElementById("rmbGot").value;
+  var excRate = document.getElementById("excRate").value;
+  //console.log(rmbGot);
+  //console.log(excRate);
+  maxRmb = calcLoop(rmbGot, excRate);
+  document.getElementById("maxRmb").value = maxRmb;
+  var discountRmb = maxRmb - 0.01;
+  while(maxRmb - calcLoop(discountRmb, excRate) < 0.01){
+    discountRmb -= 0.01;
+  }
+  document.getElementById("discountRmb").value = discountRmb;
 }
 
 autoExc()
