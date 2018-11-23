f2451309bff64c513d5d3c9dbceae02345c312be
MA Junyi <mjysci@live.com>
2018-11-23 12:20:07 +0800

    Site updated: 2018-11-23 12:20:07

--- cardcalc/js/calc.js
+++ cardcalc/js/calc.js
@@ -10,7 +10,7 @@ function autoExc() {
   $.ajax({
     type: "get",
     async: false,
-    url: 'http://api.k780.com:88/?app=finance.rate&scur=USD&tcur=CNY&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json&jsoncallback=data=?',
+    url: 'https://sapi.k780.com/?app=finance.rate&scur=USD&tcur=CNY&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json&jsoncallback=data=?',
     dataType: "jsonp",
     success: function(data) {
       $("#excRate").val(data.result.rate)
