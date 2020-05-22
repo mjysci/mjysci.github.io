79c7de6e84d140430669353936811be1fe795120
MA Junyi <mjysci@live.com>
2020-05-22 11:08:48 +0800

    Site updated: 2020-05-22 11:08:45

---
+++ tools/microsoft-graph-explorer/precache-manifest.f5c04f5898d80b85b8ffdc055c9c92ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c1175b7f3ade080487d",
+    "url": "/tools/microsoft-graph-explorer/static/css/graph-explorer-v2.css"
+  },
+  {
+    "revision": "7c1175b7f3ade080487d",
+    "url": "/tools/microsoft-graph-explorer/static/js/graph-explorer-v2.js"
+  },
+  {
+    "revision": "896a0924ffee9501ec49dadf2f7e35bb",
+    "url": "/tools/microsoft-graph-explorer/editor.worker.js"
+  },
+  {
+    "revision": "edc8d66650dac200f66013b010419b69",
+    "url": "/tools/microsoft-graph-explorer/json.worker.js"
+  },
+  {
+    "revision": "d7e34b9652b848582ae43b8f47d30880",
+    "url": "/tools/microsoft-graph-explorer/index.html"
+  }
+];
