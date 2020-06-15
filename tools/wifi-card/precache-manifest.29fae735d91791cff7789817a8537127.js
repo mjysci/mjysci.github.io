3519f399e6438c15b7bc7bb9dfc3126fd7e06f28
MA Junyi <mjysci@live.com>
2020-06-15 09:05:01 +0800

    Site updated: 2020-06-15 09:04:59

---
+++ tools/wifi-card/precache-manifest.29fae735d91791cff7789817a8537127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44333d0e35247186fd4c0c5bdc4a1533",
+    "url": "/tools/wifi-card/index.html"
+  },
+  {
+    "revision": "48cdf35aa6b83bf4792e",
+    "url": "/tools/wifi-card/static/css/main.74e04ca0.chunk.css"
+  },
+  {
+    "revision": "b78348e3859f5c338c70",
+    "url": "/tools/wifi-card/static/js/2.9bca3d5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tools/wifi-card/static/js/2.9bca3d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48cdf35aa6b83bf4792e",
+    "url": "/tools/wifi-card/static/js/main.7ec661fa.chunk.js"
+  },
+  {
+    "revision": "cc3049058dbd9e1969db",
+    "url": "/tools/wifi-card/static/js/runtime-main.fa7e2d5e.js"
+  }
+]);
---
+++ tools/wifi-card/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tools/wifi-card/precache-manifest.29fae735d91791cff7789817a8537127.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tools/wifi-card/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
